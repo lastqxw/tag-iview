3f818af7492897430fe1b25fe617a0a023c3c6d3
las_t <2213958088@qq.com>
2018-11-08 17:01:52 +0800

    修改标签口径查询页面

--- src/service/labelQuery.js
+++ src/service/labelQuery.js
@@ -4,13 +4,13 @@ export default {
 	methods: {
         // 标签模糊搜索
         selectAllLable(param) {
-			return Util.get('lable/selectAllLable', param).then(res => {
+			return Util.get('lable/lableTree', param).then(res => {
 				return Util.result(res.data,"隐藏")
 			})
 		},
 		// g根据标签code搜索http://192.168.10.196:8080/biaoqian_admin/lable/selectAllLable1
 		selectAllLable1(param) {
-			return Util.get('lable/selectAllLable1', param).then(res => {
+			return Util.get('lable/lableTree1', param).then(res => {
 				return Util.result(res.data,"查询")
 			})
 		},
